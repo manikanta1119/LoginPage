0abd64b938d73dfc28e61532db6bb94cb25cd279
manikanta1119 <45948056+manikanta1119@users.noreply.github.com>
2020-05-16 20:27:43 -0500

    Add project files.

---
+++ LoginPage/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            DemoEntities Db = new DemoEntities();
+            customer model = new customer();
+            GetCustomer();
+            
+           return View();
+        }
+
+        public ActionResult GetCustomer()
+        {
+            int page =0;
+            int rows =0;
+            DemoEntities db = new DemoEntities();
+            var data = db.customers.OrderBy(x=>x.customer_id);
+           
+            int pageIndex = Convert.ToInt32(page) - 1;
+            int pageSize = rows;
+            int totalRecords = data.Count();
+            int totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
+            
+            var jsonData = new
+            {
+                total = totalPages,
+                page = page,
+                records = totalRecords,
+                rows = data
+            };
+            return Redirect("Edit",data);
+        }
+        
+        public ActionResult Edit(int custmoerid)
+        {
+            DemoEntities Demo = new DemoEntities();
+
+            var Data = Demo.customers.OrderBy(x => x.customer_id == custmoerid).FirstOrDefault();
+            return View(Data);
+
+
+        }
+     
+    }
+}
+
---
+++ LoginPage/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(LoginPage.Models.UserCradential userCradential)
+        {
+            using (DemoEntities db = new DemoEntities())
+            {
+                var UserDetails = db.UserCradentials.Where(x => x.UserName == userCradential.UserName && x.Password == userCradential.Password).FirstOrDefault();
+                if(UserDetails == null)
+                {
+                    userCradential.LoginErrorMessage = "Wrong UserName or Password";
+
+                    return View("Index", userCradential);
+                }
+                else
+                {
+                    Session["UserId"] = UserDetails.UserID;
+                    return RedirectToAction("GetCustomer", "Home");
+                }
+            }
+                
+        }
+
+
+    }
+}
